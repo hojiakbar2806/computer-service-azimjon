c407d64f391d6b1c02e8f90a62571c5e97f9c646
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-05-30 17:53:46 +0500

    fix db

--- backend/src/utils/mongodb.js
+++ backend/src/utils/mongodb.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb"
 
-const uri = "mongodb+srv://azimjonabdumannonov43:Tm3VCJ1HBLiAv12c@cluster0.ydk0hza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://azimjonabdumannonov43:1iwAnbCYLqEhmTrN@cluster0.ydk0hza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const client = new MongoClient(uri)
 
 const db = client.db("service")
